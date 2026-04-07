f14db42a7e4ab8ca35e958508456a038a64e3c58
Lake Mauer <lmauer1@uiowa.edu>
2026-04-06 22:42:42 -0500

    Added Style and Template

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # module 10 - Flask Application
 # Lake Mauer
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
